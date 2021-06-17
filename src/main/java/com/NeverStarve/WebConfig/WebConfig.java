0fcec7cdc695806150fa616286fd571760d477f5
ray870317 <ray870317@gmail.com>
2021-06-17 14:14:23 +0800

    完成店家登入Cookie驗證

--- src/main/java/com/NeverStarve/WebConfig/WebConfig.java
+++ src/main/java/com/NeverStarve/WebConfig/WebConfig.java
@@ -1,13 +1,20 @@
 package com.NeverStarve.WebConfig;
 
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.NeverStarve.store.interceptor.StoreLoginInterceptor;
+
+
 
 public class WebConfig  implements WebMvcConfigurer{
 	
-	
-	
-	
-	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new StoreLoginInterceptor())
+				.addPathPatterns("/store/**")
+				.excludePathPatterns("/store/login");
+	}
 	
 	
 }
