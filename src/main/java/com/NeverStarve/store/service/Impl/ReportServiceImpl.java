830b95ba2fde4fca6694821d64b69c1ab62aa9f0
mike <a57249683012@gmail.com>
2021-06-21 15:38:57 +0800

    修改bug

--- src/main/java/com/NeverStarve/store/service/Impl/ReportServiceImpl.java
+++ src/main/java/com/NeverStarve/store/service/Impl/ReportServiceImpl.java
@@ -2,9 +2,12 @@ package com.NeverStarve.store.service.Impl;
 
 import java.util.List;
 
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.NeverStarve.orders.model.OrderBean;
 import com.NeverStarve.orders.model.OrderListBean;
@@ -12,7 +15,8 @@ import com.NeverStarve.orders.repository.OrderRepository;
 import com.NeverStarve.store.model.StoreBean;
 import com.NeverStarve.store.repository.StoreRepository;
 import com.NeverStarve.store.service.ReportService;
-
+@Service
+@Transactional
 public class ReportServiceImpl implements ReportService {
 	
 	@Autowired
@@ -31,9 +35,14 @@ public class ReportServiceImpl implements ReportService {
 	public List<OrderListBean> getOrderListAll(HttpSession session) {
 		Integer pkStoreId1=getpkStoreId(session); //透過session 找到對應的鬍鬚張ID
 		StoreBean storeBean = storeRepository.getById(pkStoreId1); //透過 鬍鬚張ID找到對應的鬍鬚張的StoreBean
-		OrderBean orderBean= (OrderBean) orderRepository.findByStoreBean(storeBean);//透過鬍鬚張的storebean找到屬於鬍鬚張的訂單
-		OrderListBean  orderListBean=(OrderListBean) orderBean.getOrderListBean();//透過鬍鬚張的訂單找到鬍鬚張的訂單詳細
-		return (List<OrderListBean>) orderListBean;
+		List<OrderBean> orderBean= orderRepository.findByStoreBean(storeBean);//透過鬍鬚張的storebean找到屬於鬍鬚張的訂單，有很多個訂單所以
+		//要用list去存
+		List<OrderListBean> orderListBean = null;
+		for( OrderBean var: orderBean  ) { //將每筆訂單的訂單詳細存入orderListBean裡面
+			orderListBean.add((OrderListBean) var.getOrderListBean());  
+		}
+//		List<OrderListBean>  orderListBean= orderBean.getOrderListBean();//透過鬍鬚張的訂單找到鬍鬚張的訂單詳細
+		return orderListBean;
 	}
 
 	
