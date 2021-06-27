400583325bb09469535479f4c83e9bceb0f85c90
Kevin <kao850702@gmail.com>
2021-06-27 13:58:47 +0800

    更新改成PUT 但仍會有 不預期的錯誤驗證 只好將其先關閉 後台使用攔截器 做簡單的權限驗證

---
+++ src/main/java/com/NeverStarve/backStage/interceptor/CheckLoginlnterceptor.java
@@ -0,0 +1,18 @@
+package com.NeverStarve.backStage.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CheckLoginlnterceptor implements WebMvcConfigurer {
+	
+	@Autowired
+	AdminInterceptor adm;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(adm).addPathPatterns("/Backstage/**");
+	}
+}
