65877ddd8b901aaef0b6361a1724eb9a86ab9f97
Tony <boy35679@gmail.com>
2021-07-01 09:34:35 +0800

    修改預約系統 預約頁面加入會員資訊(尚未完成) 導覽列加入預約系統連結

--- src/main/java/com/NeverStarve/booking/controller/BookingController.java
+++ src/main/java/com/NeverStarve/booking/controller/BookingController.java
@@ -8,19 +8,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.NeverStarve.booking.model.BookingTableBean;
 import com.NeverStarve.booking.service.BookingService;
+import com.NeverStarve.member.model.MemberBean;
 
 @Controller
 @RequestMapping("/Booking")
+@SessionAttributes({"member"})
 public class BookingController {
 	
 	@Autowired
 	private BookingService bookingService;
 
 	@GetMapping("/")
-	public String toBookingTable() {
+	public String toBookingTable(Model model, SessionStatus status) {
+		
+		MemberBean memberBean = (MemberBean) model.getAttribute("member");
+		if (memberBean == null) {
+			status.setComplete();
+			return "redirect:/Member/login";
+		}
 		
 		return "booking/bookingTable";
 	}
